8f03c10d9d11de1d7be5dd2d1469d1f9fc430e4b
Developer <test@example.com>
2026-08-24 20:14:00 +0700

    Import only cyrillic font subsets (-200KB fonts), disable modulePreload to prevent lazy chunk preloading

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: { "@": path.resolve(__dirname, "src") },
   },
   build: {
+    modulePreload: false,
     rollupOptions: {
       output: {
         manualChunks: {
